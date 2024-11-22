562737da6086942bf865f1d566812978e3d9fba2
immortalWebDev <pgbadgujar007@gmail.com>
2024-11-22 17:20:04 +0530

    Fetched cart length from state logic and updated style little

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,96 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const state = useSelector((state) => state.cart);
+
   return (
     <nav className="navbar">
-      <NavLink className="" to="/">
-        <i className="fa fa-shopping-bag me-1"></i> Shopping mart
-      </NavLink>
+      
+        <NavLink
+          className=""
+          to="/"
+        
+        >
+          <i className="fa fa-shopping-bag me-1"></i> Shopping mart
+        </NavLink>
 
-      <button className="navbar-toggler">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+        
+        <button
+          className="navbar-toggler"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <div className="nabbar-content">
-        <ul className="navbar-nav text-center">
-          <li className="nav-item px-4">
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li className="nav-item px-2">
-            <NavLink to="/product">Products</NavLink>
-          </li>
-          <li className="nav-item px-2">
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li className="nav-item px-2">
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
+        
+        <div className="nabbar-content" id="navbarContent">
+          
+          <ul className="navbar-nav text-center">
+            <li className="nav-item px-4">
+              <NavLink
+                to="/"
+                
+                  
+                    
+                
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item px-3">
+              <NavLink
+                to="/product"
+               
+              >
+                Products
+              </NavLink>
+            </li>
+            <li className="nav-item px-3">
+              <NavLink
+                to="/about"
+               
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item px-3">
+              <NavLink
+                to="/contact"
+                
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
 
-        <div className="buttons text-center">
-          <NavLink to="/login">
-            <i className="fa fa-user me-2"></i>Login
-          </NavLink>
-          <NavLink to="/register">
-            <i className="fa fa-check me-2"></i>Register
-          </NavLink>
-          <NavLink to="/cart">
-            <i className="me-2"></i>Cart
-          </NavLink>
+          {/* Buttons */}
+          <div className="buttons text-center">
+            <NavLink
+              to="/login"
+              
+             
+            >
+              <i className="fa fa-user me-2"></i>Login
+            </NavLink>
+            <NavLink
+              to="/register"
+              
+             
+            >
+              <i className="fa fa-check me-2"></i>Register
+            </NavLink>
+            <NavLink
+              to="/cart"
+              
+             
+            >
+              <i className="me-2"></i>Cart ({state.length}
+              )
+            </NavLink>
+          </div>
         </div>
-      </div>
+      
     </nav>
   );
 };
