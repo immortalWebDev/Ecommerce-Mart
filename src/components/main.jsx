0888d2a1d5e3e6627dcac47e11cdf7c67f93c799
immortalWebDev <pgbadgujar007@gmail.com>
2024-11-22 16:59:26 +0530

    main component push for Home

---
+++ src/components/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="main border-1">
+        <div className="card bg-dark text-white border-0 mx-3">
+          <div className="align-items-center">
+            <h3 className="fs-1 text-primary fw-bold ml-3">My deals!</h3>
+            <p className="ml-3">Your deals are waiting fast</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
