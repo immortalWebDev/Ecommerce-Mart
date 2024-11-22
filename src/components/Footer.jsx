37a775a970ceaa720374d05d3f146f4c2c3b4c2c
immortalWebDev <pgbadgujar007@gmail.com>
2024-11-22 17:04:36 +0530

    Added temp links for git and portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+
 const Footer = () => {
   return (
     <>
+    
       <footer className="">
         <div className="d-flex flex-column justify-content-center">
           <div className="">
             <p className="mb-md-3 mb-2 text-muted">
+              
               <a
                 href="my portfolio link"
                 className="text-primary fw-bold"
@@ -16,9 +19,36 @@ const Footer = () => {
                 Piyush Badgujar
               </a>
             </p>
+            <a
+              href="my git link"
+              className="fs-4 text-dark mx-3"
+              target="_blank"
+              rel="noreferrer"
+              
+            >
+              <i
+                className="fa fa-github"
+                aria-hidden="true"
+                
+              ></i>
+            </a>
+
+            <a
+              href="my portfolio link"
+              className="fs-4 text-dark mx-3"
+              target="_blank"
+              rel="noreferrer"
+              
+            >
+              <i
+                className="fa fa-star"
+                aria-hidden="true"
+              
+              ></i>
+            </a>
           </div>
           <span className="mt-3 fs-6 text-secondary">
-            copyright 2024 Rights reserved
+            © 2024 Rights reserved
           </span>
         </div>
       </footer>
