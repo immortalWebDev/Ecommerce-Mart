baccc33863e073c6643b25c8b24a1a63452d6399
immortalWebDev <pgbadgujar007@gmail.com>
2024-11-24 18:04:10 +0530

    Added products component (sort of main)

---
+++ src/components/Products.jsx
@@ -0,0 +1,129 @@
+import React, { useState, useEffect, useRef } from "react";
+import { addCart } from "../redux/slices/cartSlice";
+
+const Products = () => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const componentMounted = useRef(true);
+  const dispatch = useDispatch();
+
+  const addProduct = (product) => {
+    dispatch(addCart(product));
+  };
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch(`${process.env.REACT_APP_STORE_URL}`);
+      if (componentMounted.current) {
+        setData(await response.clone().json());
+        setFilter(await response.json());
+      }
+
+      return () => {
+        componentMounted.current = false;
+      };
+    };
+
+    getProducts();
+  }, []);
+
+  const Loading = () => {
+    return (
+      <div className="row">
+        {[...Array(6)].map((_, i) => (
+          <div key={i} className="col-md-4 col-sm-6 mb-4"></div>
+        ))}
+      </div>
+    );
+  };
+
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="d-flex flex-wrap  gap-3 py-3">
+          <button
+            className="btn btn-gradient btn-sm text-dark px-4 py-2"
+            onClick={() => filterProduct("men's clothing")}
+          >
+            CAT 1
+          </button>
+          <button
+            className="btn btn-gradient btn-sm text-dark px-4 py-2"
+            onClick={() => filterProduct("women's clothing")}
+          >
+            CAT 2
+          </button>
+          <button
+            className="btn btn-gradient btn-sm text-dark px-4 py-2"
+            onClick={() => filterProduct("jewelery")}
+          >
+            CAT 3
+          </button>
+          <button
+            className="btn btn-gradient btn-sm text-dark px-4 py-2"
+            onClick={() => filterProduct("electronics")}
+          >
+            CAt 4
+          </button>
+        </div>
+
+        <div className="row">
+          {filter.map((product) => {
+            return (
+              <div
+                id={product.id}
+                key={product.id}
+                className="col-md-4 col-sm-6 "
+              >
+                <div className="card text-center h-100">
+                  <img
+                    className=""
+                    src={product.image}
+                    alt={product.title}
+                    height={300}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title fw-bold">
+                      {product.title.substring(0, 15)}...
+                    </h5>
+                    <p className="card-text text-muted small">
+                      {product.description.substring(0, 90)}...
+                    </p>
+                  </div>
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-ite">$ {product.price}</li>
+                  </ul>
+                  <div className="card-body">
+                    <Link to="/checkout">Buy Now</Link>
+                    <button
+                      className="btn btn-outline-primary"
+                      onClick={() => {
+                        addProduct(product);
+                      }}
+                    >
+                      Add to Cart
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 text-center">
+          <p className="text-muted">Headline 2 ....</p>
+          <hr className="w-50 mx-auto text-success" />
+        </div>
+      </div>
+      <div className="row justify-content-center">Main content</div>
+    </div>
+  );
+};
+
+export default Products;
