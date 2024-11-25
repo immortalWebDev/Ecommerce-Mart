d165a02ccdc96e681d403cf4feaf3ac25adf7801
immortalWebDev <pgbadgujar007@gmail.com>
2024-11-25 10:55:59 +0530

    Updated styling (css modules) (Also fetched all cats from store)

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,44 +7,68 @@ const AboutPage = () => {
       <Navbar />
       <div className="container my-3 py-3">
         <h1 className="text-center text-primary display-5">Welcome</h1>
-
-        <p className="lead text-center">
-          Paragraph of intro rough 300-400 chars
+        
+        <p
+          className="lead text-center"
+          
+        >
+          Paragraph of intro rough 300-400
         </p>
 
-        <h2 className="text-center  pb-3">categories</h2>
+        <h2 className="text-center text-info pt-5 pb-3">Our main categories</h2>
         <div className="row">
-          <div className="">
-            <div className="">
+          <div className={`col-md-3 col-sm-6 mb-4 px-3 ${styles['card']}`}>
+            <div className={`h-100 shadow-lg border-0 ${styles['card']}`}>
               <img
-                className=""
+                className={`img-fluid rounded-top ${styles['card-img-top']}`}
                 src="\assets\womens-clothing.jpg"
-                alt="Women's fash"
+                alt="Women's Clothing"
+                height={160}
               />
-              <div className="">
-                <h5 className="women-fashion">Women fashion</h5>
+              <div className={`text-center ${styles['card-body']}`}>
+                <h5 className={`fw-bold ${styles[`card-title`]}`}>Her Fashion</h5>
               </div>
             </div>
           </div>
 
-          <div className="">
-            <div className="">
-              <img className="" src="\assets\jewelry.jpg" alt="Jewelry" />
-              <div className="">
-                <h5 className="">Gems & Adornments</h5>
-              </div>
-            </div>
-          </div>
-
-          <div className="">
-            <div className="">
+          <div className={`col-md-3 col-sm-6 mb-4 px-3 ${styles['card']}`}>
+            <div className={`h-100 shadow-lg border-0 ${styles['card']}`}>
               <img
-                className=""
+                className={`img-fluid rounded-top ${styles['card-img-top']}`}
+                src="\assets\jewelry.jpg"
+                alt="Jewelry"
+                height={160}
+              />
+              <div className={`text-center ${styles['card-body']}`}>
+                <h5 className={`fw-bold ${styles[`card-title`]}`}>Gems & Adornments</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className={`col-md-3 col-sm-6 mb-4 px-3 ${styles['card']}`}>
+            <div className={`h-100 shadow-lg border-0 ${styles['card']}`}>
+              <img
+                className={`img-fluid rounded-top ${styles['card-img-top']}`}
                 src="\assets\mens-clothing.jpg"
                 alt="Men's Clothing"
+                height={160}
               />
-              <div className="">
-                <h5 className="">Men Fashion</h5>
+              <div className={`text-center ${styles['card-body']}`}>
+                <h5 className={`fw-bold ${styles[`card-title`]}`}>His Fashion</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className={`col-md-3 col-sm-6 mb-4 px-3 ${styles['card']}`}>
+            <div className={`h-100 shadow-lg border-0 ${styles['card']}`}>
+              <img
+                className={`img-fluid rounded-top ${styles['card-img-top']}`}
+                src="\assets\comp-electronics.jpeg"
+                alt="Electronics"
+                height={160}
+              />
+              <div className={`text-center ${styles['card-body']}`}>
+                <h5 className={`fw-bold ${styles[`card-title`]}`}>Gadgets</h5>
               </div>
             </div>
           </div>
