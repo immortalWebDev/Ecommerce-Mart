dac15b53619f0755899f8a9553de2b7562b96df4
immortalWebDev <pgbadgujar007@gmail.com>
2024-11-29 17:31:16 +0530

    Components and pages export files

---
+++ src/pages/pagesExpo.js
@@ -0,0 +1,12 @@
+export { default as Home } from './Home';
+export { default as MainContent } from './MainContent';
+export { default as BuyNow} from './BuyNow';
+export { default as AboutPage } from './AboutPage';
+export { default as ContactPage } from './ContactPage';
+export { default as Cart } from './Cart';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+export { default as Checkout } from './Checkout';
+export { default as PageNotFound404Error } from './PageNotFound404Error';
+export { default as Profile } from './Profile';
+export { default as ThankYou } from './ThankYou';
