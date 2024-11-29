76ed67884ccb8d1a3bf40abed92c02a567480ead
immortalWebDev <pgbadgujar007@gmail.com>
2024-11-29 17:15:42 +0530

    Updated logic for initial cart, err handling fixes

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -1,14 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const persistCartToStorage = (cart) => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+const getInitialCart = () => {
+  const storedCart = localStorage.getItem("cart");
+  try {
+    return storedCart ? JSON.parse(storedCart) : [];
+  } catch (e) {
+    console.error("Error parsing cart data", e);
+    return []; //if parsing fails
+  }
+};
+
 const cartSlice = createSlice({
   name: "cart",
-  initialState: JSON.parse(localStorage.getItem("cart")),
+  initialState: getInitialCart(),
   reducers: {
     addCart: (state, action) => {
       const product = action.payload;
       const existingItem = state.find((item) => item.id === product.id);
       if (existingItem) {
-        existingItem.qty = existingItem.qty + 1;
+        existingItem.qty += 1;
       } else {
         state.push({ ...product, qty: 1 });
       }
@@ -19,14 +33,23 @@ const cartSlice = createSlice({
       const index = state.findIndex((item) => item.id === product.id);
       if (index !== -1) {
         if (state[index].qty > 1) {
-          state[index].qty = state[index].qty - 1;
+          state[index].qty -= 1;
         } else {
           state.splice(index, 1);
         }
       }
+      persistCartToStorage(state);
+    },
+    setCart: (state, action) => {
+      persistCartToStorage(action.payload); 
+      return action.payload;
+    },
+    clearCart: (state) => {
+      localStorage.removeItem("cart");
+      return [];
     },
   },
 });
 
-export const { addCart, delCart } = cartSlice.actions;
+export const { addCart, delCart, clearCart, setCart } = cartSlice.actions;
 export default cartSlice.reducer;
