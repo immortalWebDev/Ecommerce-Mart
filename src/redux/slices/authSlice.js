86fb28cb0b371e424c96cfda5a43e0b8bba41315
immortalWebDev <pgbadgujar007@gmail.com>
2024-11-29 16:45:14 +0530

    Updatd authslice for ref token and bugs

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -4,6 +4,7 @@ const initialAuthState = {
   isAuthenticated: JSON.parse(localStorage.getItem("isAuthenticated")) || false,
   token: localStorage.getItem("token") || null,
   userEmail: localStorage.getItem("userEmail") || null,
+  userName: localStorage.getItem("userName") || null,
 };
 
 const authSlice = createSlice({
@@ -11,23 +12,39 @@ const authSlice = createSlice({
   initialState: initialAuthState,
   reducers: {
     login: (state, action) => {
-      const { token, email } = action.payload;
+
+      const { token, email ,refreshToken,displayName,expiresIn} = action.payload;
 
       state.isAuthenticated = true;
       state.token = token;
       state.userEmail = email;
+      state.refreshToken = refreshToken;
+      state.userName = displayName;
+
 
       localStorage.setItem("isAuthenticated", true);
       localStorage.setItem("userEmail", email);
       localStorage.setItem("token", token);
+      localStorage.setItem('tokenExpiry', Date.now() + expiresIn * 1000);
+      localStorage.setItem("refreshToken", refreshToken);
+      localStorage.setItem("userName", displayName);
     },
     logout: (state) => {
+
       state.isAuthenticated = false;
       state.token = null;
+      state.refreshToken = null;
+      state.userEmail = null;
+      state.userName = null;
 
       localStorage.removeItem("isAuthenticated");
       localStorage.removeItem("token");
       localStorage.removeItem("userEmail");
+      localStorage.removeItem("cart");
+      localStorage.removeItem("userProfile");
+      localStorage.removeItem("refreshToken");
+      localStorage.removeItem("tokenExpiry");
+      localStorage.removeItem("userName");
     },
   },
 });
