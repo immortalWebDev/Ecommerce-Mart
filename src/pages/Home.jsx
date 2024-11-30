10748d2d17096be1790a93c6c803794ed362f799
immortalWebDev <pgbadgujar007@gmail.com>
2024-11-30 15:06:04 +0530

    Added Main and Home (Can flip code but its fine)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Navbar, Main, Products, Footer } from "../components/componentsExpo";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Products />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/MainContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Footer, Navbar, Products } from "../components/componentsExpo"
+
+const MainContent = () => {
+  return (
+    <>
+      <Navbar />
+      <Products />
+      <Footer />
+    </>
+  )
+}
+
+export default MainContent
