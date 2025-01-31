6679323ab6894a13aea6b7d16a1bea6320cca91b
immortalWebDev <pgbadgujar007@gmail.com>
2025-01-31 18:59:09 +0530

    Add maintenance page and toggle for maintenance mode

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import store from "./redux/store";
 import App from "./App";
 import "./bootstrap-custom.scss";
 import { NetworkCheck } from "./pages/pagesExpo";
+import MaintenancePage from "./pages/MaintenancePage/MaintenancePage";
+
+const isMaintenanceMode = true
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <NetworkCheck>
-        <App />
+      {isMaintenanceMode ? <MaintenancePage/> : <App />}
       </NetworkCheck>
     </BrowserRouter>
   </Provider>
