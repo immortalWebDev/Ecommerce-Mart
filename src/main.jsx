118c54da10bd9e19955783aae09afc99c5cfb3e3
immortalWebDev <pgbadgujar007@gmail.com>
2025-03-28 19:00:15 +0530

    Update maintenance mode flag and import Font Awesome CSS

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,11 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App";
 import "./bootstrap-custom.scss";
+import 'font-awesome/css/font-awesome.min.css';
 import { NetworkCheck } from "./pages/pagesExpo";
 import MaintenancePage from "./pages/MaintenancePage/MaintenancePage";
 
-const isMaintenanceMode = true
+const isMaintenanceMode = false
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
